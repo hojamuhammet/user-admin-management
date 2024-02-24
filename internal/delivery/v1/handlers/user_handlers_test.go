d379c33c62bec69eeb11614f198dbfd57992679f
kemvl <atdayewkemal@gmail.com>
2024-02-25 00:37:27 +0500

    some unit tests are added

--- internal/delivery/v1/handlers/user_handlers_test.go
+++ internal/delivery/v1/handlers/user_handlers_test.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -170,6 +171,308 @@ func TestCreateUserHandler(t *testing.T) {
 			router.ServeHTTP(rr, req)
 			assert.Equal(t, tt.expectedStatus, rr.Code)
 			assert.Equal(t, tt.expectedBody, strings.TrimSpace(rr.Body.String()))
+		})
+	}
+}
+
+func TestUpdateUserHandler(t *testing.T) {
+	tests := []struct {
+		name            string
+		requestBody     *domain.UpdateUserRequest
+		mockUserService func() *mocks.MockUserService
+		expectedStatus  int
+		expectedBody    string
+	}{
+		{
+			name: "successful request",
+			requestBody: &domain.UpdateUserRequest{
+				FirstName:       "Kemal",
+				LastName:        "Atdayew",
+				Gender:          "Male",
+				DateOfBirth:     dateOfBirth,
+				Location:        "Ashgabat",
+				Email:           "atdayewkemal@gmail.com",
+				ProfilePhotoURL: "https://example.com/profile.jpg",
+			},
+			mockUserService: func() *mocks.MockUserService {
+				userService := &mocks.MockUserService{}
+				userService.On("UpdateUser", mock.Anything, mock.Anything).Return(&domain.UpdateUserResponse{
+					ID:              1,
+					FirstName:       "Kemal",
+					LastName:        "Atdayew",
+					PhoneNumber:     "+99362008971",
+					Gender:          "Male",
+					DateOfBirth:     dateOfBirth,
+					Location:        "Ashgabat",
+					Email:           "atdayewkemal@gmail.com",
+					ProfilePhotoURL: "https://example.com/profile.jpg"}, nil)
+				return userService
+			},
+			expectedStatus: http.StatusOK,
+			expectedBody:   `{"id":1,"first_name":"Kemal","last_name":"Atdayew","phone_number":"+99362008971","blocked":false,"gender":"Male","registration_date":"0001-01-01T00:00:00Z","date_of_birth":"2000-01-01T00:00:00Z","location":"Ashgabat","email":"atdayewkemal@gmail.com","profile_photo_url":"https://example.com/profile.jpg"}`,
+		},
+		{
+			name: "user not found",
+			requestBody: &domain.UpdateUserRequest{
+				FirstName:       "Kemal",
+				LastName:        "Atdayew",
+				Gender:          "Male",
+				DateOfBirth:     dateOfBirth,
+				Location:        "Ashgabat",
+				Email:           "atdayewkemal@gmail.com",
+				ProfilePhotoURL: "https://example.com/profile.jpg",
+			},
+			mockUserService: func() *mocks.MockUserService {
+				userService := &mocks.MockUserService{}
+				userService.On("UpdateUser", mock.Anything, mock.Anything).Return(&domain.UpdateUserResponse{}, domain.ErrUserNotFound)
+				return userService
+			},
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   `{"status":404,"message":"User not found"}`,
+		},
+		{
+			name: "email already in use",
+			requestBody: &domain.UpdateUserRequest{
+				FirstName:       "Kemal",
+				LastName:        "Atdayew",
+				Gender:          "Male",
+				DateOfBirth:     dateOfBirth,
+				Location:        "Ashgabat",
+				Email:           "atdayewkemal@gmail.com",
+				ProfilePhotoURL: "https://example.com/profile.jpg",
+			},
+			mockUserService: func() *mocks.MockUserService {
+				userService := &mocks.MockUserService{}
+				userService.On("UpdateUser", mock.Anything, mock.Anything).Return(&domain.UpdateUserResponse{}, domain.ErrEmailInUse)
+				return userService
+			},
+			expectedStatus: http.StatusConflict,
+			expectedBody:   `{"status":409,"message":"Email already in use"}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			requestBody, err := json.Marshal(tt.requestBody)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			req, err := http.NewRequest("PUT", "/api/user/1", bytes.NewBuffer(requestBody))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			router := chi.NewRouter()
+
+			mockUserService := tt.mockUserService()
+
+			handler := &handlers.UserHandler{
+				UserService: mockUserService,
+			}
+
+			router.Put("/api/user/{id}", handler.UpdateUserHandler)
+
+			router.ServeHTTP(rr, req)
+
+			if status := rr.Code; status != tt.expectedStatus {
+				t.Errorf("handler returned wrong status code: got %v want %v", status, tt.expectedStatus)
+			}
+
+			if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(tt.expectedBody) {
+				t.Errorf("handler returned unexpected body: got %v want %v", []byte(strings.TrimSpace(rr.Body.String())), []byte(strings.TrimSpace(tt.expectedBody)))
+			}
+		})
+	}
+}
+
+func TestDeleteUserHandler(t *testing.T) {
+	tests := []struct {
+		name            string
+		id              int
+		mockUserService func() *mocks.MockUserService
+		expectedStatus  int
+		expectedBody    string
+	}{
+		{
+			name: "successful request",
+			id:   1,
+			mockUserService: func() *mocks.MockUserService {
+				userService := &mocks.MockUserService{}
+				userService.On("DeleteUser", mock.Anything).Return(nil)
+				return userService
+			},
+			expectedStatus: http.StatusOK,
+			expectedBody:   `{"status":200,"message":"User deleted successfully"}`,
+		},
+		{
+			name: "user not found",
+			id:   1,
+			mockUserService: func() *mocks.MockUserService {
+				userService := &mocks.MockUserService{}
+				userService.On("DeleteUser", mock.Anything).Return(domain.ErrUserNotFound)
+				return userService
+			},
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   `{"status":404,"message":"User not found"}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest("DELETE", fmt.Sprintf("/api/user/%d", tt.id), nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			router := chi.NewRouter()
+
+			mockUserService := tt.mockUserService()
+
+			handler := &handlers.UserHandler{
+				UserService: mockUserService,
+			}
+
+			router.Delete("/api/user/{id}", handler.DeleteUserHandler)
+
+			router.ServeHTTP(rr, req)
+
+			if status := rr.Code; status != tt.expectedStatus {
+				t.Errorf("handler returned wrong status code: got %v want %v", status, tt.expectedStatus)
+			}
+
+			if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(tt.expectedBody) {
+				t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), tt.expectedBody)
+			}
+		})
+	}
+}
+
+func TestBlockUserHandler(t *testing.T) {
+	tests := []struct {
+		name            string
+		id              int
+		mockUserService func() *mocks.MockUserService
+		expectedStatus  int
+		expectedBody    string
+	}{
+		{
+			name: "successful request",
+			id:   1,
+			mockUserService: func() *mocks.MockUserService {
+				userService := &mocks.MockUserService{}
+				userService.On("BlockUser", mock.Anything).Return(nil)
+				return userService
+			},
+			expectedStatus: http.StatusOK,
+			expectedBody:   `{"status":200,"message":"User blocked successfully"}`,
+		},
+		{
+			name: "user not found",
+			id:   1,
+			mockUserService: func() *mocks.MockUserService {
+				userService := &mocks.MockUserService{}
+				userService.On("BlockUser", mock.Anything).Return(domain.ErrUserNotFound)
+				return userService
+			},
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   `{"status":404,"message":"User not found"}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest("PUT", fmt.Sprintf("/users/%d/block", tt.id), nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			router := chi.NewRouter()
+
+			mockUserService := tt.mockUserService()
+
+			handler := &handlers.UserHandler{
+				UserService: mockUserService,
+			}
+
+			router.Put("/users/{id}/block", handler.BlockUserHandler)
+
+			router.ServeHTTP(rr, req)
+
+			if status := rr.Code; status != tt.expectedStatus {
+				t.Errorf("handler returned wrong status code: got %v want %v", status, tt.expectedStatus)
+			}
+
+			if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(tt.expectedBody) {
+				t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), tt.expectedBody)
+			}
+		})
+	}
+}
+
+func TestUnblockUserHandler(t *testing.T) {
+	tests := []struct {
+		name            string
+		id              int
+		mockUserService func() *mocks.MockUserService
+		expectedStatus  int
+		expectedBody    string
+	}{
+		{
+			name: "successful request",
+			id:   1,
+			mockUserService: func() *mocks.MockUserService {
+				userService := &mocks.MockUserService{}
+				userService.On("UnblockUser", mock.Anything).Return(nil)
+				return userService
+			},
+			expectedStatus: http.StatusOK,
+			expectedBody:   `{"status":200,"message":"User unblocked successfully"}`,
+		},
+		{
+			name: "user not found",
+			id:   1,
+			mockUserService: func() *mocks.MockUserService {
+				userService := &mocks.MockUserService{}
+				userService.On("UnblockUser", mock.Anything).Return(domain.ErrUserNotFound)
+				return userService
+			},
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   `{"status":404,"message":"User not found"}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest("PUT", fmt.Sprintf("/api/user/%d/unblock", tt.id), nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			router := chi.NewRouter()
+
+			mockUserService := tt.mockUserService()
+
+			handlers := &handlers.UserHandler{
+				UserService: mockUserService,
+			}
+
+			router.Put("/api/user/{id}/unblock", handlers.UnblockUserHandler)
+
+			router.ServeHTTP(rr, req)
+
+			if status := rr.Code; status != tt.expectedStatus {
+				t.Errorf("handler returned wrong status code: got %v want %v", status, tt.expectedStatus)
+			}
+
+			if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(tt.expectedBody) {
+				t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), tt.expectedBody)
+			}
 		})
 	}
 }
