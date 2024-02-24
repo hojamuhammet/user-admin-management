4914ba65782bce16c1e0f600e749430ab26b7b2d
kemvl <atdayewkemal@gmail.com>
2024-02-24 17:06:02 +0500

    Mocks are added for future tests

---
+++ internal/mocks/service/user_service_mock.go
@@ -0,0 +1,51 @@
+package mocks
+
+import (
+	"admin-panel/internal/domain"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type MockUserService struct {
+	mock.Mock
+}
+
+func (m *MockUserService) GetAllUsers(page, pageSize int) (*domain.UsersList, error) {
+	args := m.Called(page, pageSize)
+	return args.Get(0).(*domain.UsersList), args.Error(1)
+}
+
+func (m *MockUserService) GetUserByID(id int32) (*domain.GetUserResponse, error) {
+	args := m.Called(id)
+	return args.Get(0).(*domain.GetUserResponse), args.Error(1)
+}
+
+func (m *MockUserService) CreateUser(request *domain.CreateUserRequest) (*domain.CreateUserResponse, error) {
+	args := m.Called(request)
+	return args.Get(0).(*domain.CreateUserResponse), args.Error(1)
+}
+
+func (m *MockUserService) UpdateUser(id int32, request *domain.UpdateUserRequest) (*domain.UpdateUserResponse, error) {
+	args := m.Called(id, request)
+	return args.Get(0).(*domain.UpdateUserResponse), args.Error(1)
+}
+
+func (m *MockUserService) DeleteUser(id int32) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
+
+func (m *MockUserService) BlockUser(id int32) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
+
+func (m *MockUserService) UnblockUser(id int32) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
+
+func (m *MockUserService) SearchUsers(query string, page, pageSize int) (*domain.UsersList, error) {
+	args := m.Called(query, page, pageSize)
+	return args.Get(0).(*domain.UsersList), args.Error(1)
+}
