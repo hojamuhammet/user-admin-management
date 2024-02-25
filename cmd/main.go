1d886bd2b80bac58841522c095e8db9661d21cd4
kemvl <atdayewkemal@gmail.com>
2024-02-25 22:32:04 +0500

    extra comments are removed

--- cmd/main.go
+++ cmd/main.go
@@ -38,7 +38,7 @@ func main() {
 	log := logger.SetupLogger(cfg.Env)
 
 	slog.Info("Starting the server...", slog.String("env", cfg.Env))
-	slog.Debug("Debug messages are enabled") // If env is set to prod, debug messages are going to be disabled
+	slog.Debug("Debug messages are enabled")
 
 	db, err := database.InitDB(cfg)
 	if err != nil {
@@ -88,7 +88,6 @@ func main() {
 		httpSwagger.URL("http://localhost:8080/swagger/doc.json"),
 	))
 
-	// Handling graceful shutdown
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
